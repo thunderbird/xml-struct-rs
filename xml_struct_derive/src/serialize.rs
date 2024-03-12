f2b871220cd7eba1e84e88270d14cbc8f7ee6f7d
Seán de Búrca <leftmostcat@gmail.com>
2024-03-12 14:19:25 -0700

    Combine errors when processing struct fields

--- xml_struct_derive/src/serialize.rs
+++ xml_struct_derive/src/serialize.rs
@@ -23,12 +23,16 @@ pub(crate) fn write_serialize_impl_for_struct(
     input: DataStruct,
     props: TypeProps,
 ) -> TokenStream {
+    // We build a list of errors so that we can combine them later and emit
+    // them all instead of quitting at the first we encounter.
+    let mut errors = Vec::new();
+
     // Process the struct's fields in order to determine how to represent them,
     // based on struct type and any consumer-applied attributes.
     let fields = match input.fields {
         // Fields in a regular struct, i.e. declared with a name and type.
         syn::Fields::Named(fields) => {
-            let map_result: Result<Vec<Field>, syn::Error> = fields
+            fields
                 .named
                 .into_iter()
                 .map(|field| {
@@ -37,53 +41,51 @@ pub(crate) fn write_serialize_impl_for_struct(
                     let ident = field.ident.unwrap();
                     let accessor = quote!(self.#ident);
 
-                    let field = Field {
+                    Field {
                         kind: FieldKind::Named(ident),
                         ty: field.ty.into_token_stream(),
                         accessor,
-                        props: FieldProps::try_from_attrs(field.attrs, true)?,
-                    };
-
-                    Ok(field)
+                        props: FieldProps::try_from_attrs(field.attrs, true)
+                            .unwrap_or_else(collect_field_processing_error(&mut errors)),
+                    }
                 })
-                .collect();
-
-            match map_result {
-                Ok(fields) => fields,
-                Err(err) => return err.into_compile_error(),
-            }
+                .collect()
         }
 
         // Fields in a tuple struct, i.e. declared by type and position only.
-        syn::Fields::Unnamed(fields) => {
-            let map_result: Result<Vec<Field>, syn::Error> = fields
-                .unnamed
-                .into_iter()
-                .enumerate()
-                .map(|(idx, field)| {
-                    let idx_literal = Literal::usize_unsuffixed(idx);
-                    let accessor = quote!(self.#idx_literal);
+        syn::Fields::Unnamed(fields) => fields
+            .unnamed
+            .into_iter()
+            .enumerate()
+            .map(|(idx, field)| {
+                let idx_literal = Literal::usize_unsuffixed(idx);
+                let accessor = quote!(self.#idx_literal);
 
-                    let field = Field {
-                        kind: FieldKind::Unnamed,
-                        ty: field.ty.into_token_stream(),
-                        accessor,
-                        props: FieldProps::try_from_attrs(field.attrs, false)?,
-                    };
-
-                    Ok(field)
-                })
-                .collect();
-
-            match map_result {
-                Ok(fields) => fields,
-                Err(err) => return err.into_compile_error(),
-            }
-        }
+                Field {
+                    kind: FieldKind::Unnamed,
+                    ty: field.ty.into_token_stream(),
+                    accessor,
+                    props: FieldProps::try_from_attrs(field.attrs, false)
+                        .unwrap_or_else(collect_field_processing_error(&mut errors)),
+                }
+            })
+            .collect(),
 
         // A unit struct, i.e. one which has no fields.
         syn::Fields::Unit => vec![],
     };
+
+    // Combine and return errors if there are any. If none, we've successfully
+    // handled all fields and can generate the final implementation.
+    let err = errors.into_iter().reduce(|mut acc, err| {
+        acc.combine(err);
+
+        acc
+    });
+
+    if let Some(err) = err {
+        return err.into_compile_error();
+    }
 
     generate_serialize_impl_for(ident, generics, props, with_struct_fields(fields))
 }
